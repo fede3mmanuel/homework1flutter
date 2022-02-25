e8c34a16f8f40cef84bda8743efa5dfdfef5b34b
Emmanuel Sanchez <fede@fede.website>
2022-02-24 22:29:06 -0600

    test 1 resolved

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Assignment 1'),
+        ),
+        body: Center(child: Text('This is the first assignment!')),
+      ),
+    );
+  }
+}
